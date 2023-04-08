66f16dad623f50af1883dc89c4295ab66418243e
AsanoYuta <119588850+AsanoYuta@users.noreply.github.com>
2023-04-08 19:32:30 +0000

    JAVA study

--- Main.java
+++ Main.java
@@ -1,22 +1,16 @@
 class Main {
   public static void main(String[] args) {
 
-    int[] numbers = {1, 4, 6, 9, 13, 16};
+    printData("Kate Jones", 27);
+    printData("John Christopher Smith", 65);
 
-    int oddSum = 0;
-    int evenSum = 0;
+  }
 
 
-    for (int number:numbers){
-      if (number % 2 == 0){
-       evenSum = evenSum +number;
-      }
-      else{
-       oddSum = oddSum + number;
-      }
-    }
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
 
-    System.out.println("奇数の和は" + oddSum + "です");
-    System.out.println("偶数の和は" + evenSum + "です");
+    System.out.println("年齢は" + age + "歳です");
+
   }
 }
